aba838c8da38a303a15d24694d28a86a218e3a1a
Senka107 <93744556+Pier-Alexis@users.noreply.github.com>
2025-09-11 13:04:17 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,15 +2,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Exemple de données
-computers = [
-    {"id": 1, "modele": "Dell Optiplex 7010", "etat": "SSD installé"},
-    {"id": 2, "modele": "HP EliteBook 840", "etat": "Nettoyage en cours"},
-]
-
 @app.route("/")
-def index():
-    return render_template("index.html", computers=computers)
+def home():
+    return render_template("index.html")
+
+@app.route("/login")
+def login():
+    return "<h1>Page de connexion (à coder bientôt)</h1>"
 
 if __name__ == "__main__":
     app.run(debug=True)
